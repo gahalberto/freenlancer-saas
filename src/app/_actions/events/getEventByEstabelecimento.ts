6e8c235602efb937ac3c0129285c6a3c1c1cc493
Gabriel Alberto <gahalberto@icloud.com>
2024-11-11 12:12:41 -0300

    feat: get event by userId

---
+++ src/app/_actions/events/getEventByEstabelecimento.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { db } from '@/app/_lib/prisma'
+
+export const getEventByEstabelecimento = async (userId: string) => {
+  return await db.storeEvents.findMany({
+    where: {
+      ownerId: userId,
+    },
+  })
+}
