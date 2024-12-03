f5c51275f92efa75869d0cb6db41e75ff20cce4e
Gabriel Alberto <gahalberto@icloud.com>
2024-12-03 10:12:34 -0300

    fix: stripe version api

--- src/app/_lib/stripe.ts
+++ src/app/_lib/stripe.ts
@@ -1,12 +1,12 @@
-import Stripe from 'stripe';
+import Stripe from 'stripe'
 
 if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables');
+  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables')
 }
 
 // Inicializando o Stripe com a chave de API e a versão correta
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-10-28.acacia', // Use a versão atual da API do Stripe
-});
+  apiVersion: '2024-11-20.acacia', // Use a versão atual da API do Stripe
+})
 
-export default stripe;
+export default stripe
