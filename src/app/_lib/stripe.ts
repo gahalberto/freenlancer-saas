98debb984c364940b0760c04bb740aceff1b281f
Gabriel Alberto <gahalberto@icloud.com>
2024-12-05 13:29:05 -0300

    feat: api version

--- src/app/_lib/stripe.ts
+++ src/app/_lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 
 // Inicializando o Stripe com a chave de API e a versão correta
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-10-28.acacia', // Use a versão atual da API do Stripe
+  apiVersion: '2024-11-20.acacia', // Use a versão atual da API do Stripe
 })
 
 export default stripe
