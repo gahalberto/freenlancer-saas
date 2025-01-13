c99812ecb990f1cb092896215d4cc442d7d02585
Gabriel <gahalberto@icloud.com>
2025-01-13 09:18:07 -0300

    add: calendar page to admins

---
+++ src/app/(views)/app/admin/calendario/calendarComponent.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import { useEffect, useState } from 'react'
+import { getAllServices } from '@/app/_actions/services/getAllServices'
+import { EventsServices } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+
+const CalendarComponent = () => {
+  const [events, setEvents] = useState<{ title: string; start: Date; end: Date; url?: string }[]>(
+    [],
+  )
+  const router = useRouter()
+
+  useEffect(() => {
+    async function fetchEvents() {
+      const data = await getAllServices() // Supondo que o serviço retorna os eventos
+      const formattedEvents = data.map((event) => ({
+        title: event.StoreEvents.title, // Nome do evento
+        start: event.arriveMashguiachTime,
+        end: event.endMashguiachTime,
+        url: `/app/admin/events/${event.StoreEvents.id}`, // Link para os detalhes do evento
+      }))
+      setEvents(formattedEvents)
+    }
+
+    fetchEvents()
+  }, [])
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      height="auto"
+      locale={'pt-br'}
+      eventClick={(info) => {
+        info.jsEvent.preventDefault() // Previne comportamento padrão do navegador
+        if (info.event.url) {
+          router.push(info.event.url) // Redireciona para o link do evento
+        }
+      }}
+    />
+  )
+}
+
+export default CalendarComponent
---
+++ src/app/(views)/app/admin/calendario/page.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+
+import CalendarComponent from './calendarComponent'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Calendário de Eventos</h1>
+      <CalendarComponent />
+    </div>
+  )
+}
